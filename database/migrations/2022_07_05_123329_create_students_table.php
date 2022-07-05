c9e89c26e861b9bd8409731ecacf290651ec8286
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-07-05 13:41:42 +0100

    create & migrate model controller student

---
+++ database/migrations/2022_07_05_123329_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("CNE");
+            $table->string("FirstName");
+            $table->string("SecondName");
+            $table->string("Age");
+            $table->string("Speciality");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
